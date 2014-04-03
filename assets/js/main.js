75f6fe505408bf9dbfc1f9dd058a7bfec80db4c8
Donald M. Langhorne <don22@langhorne.org>
2014-04-03 12:02:43 -0400

    fixed web links
    web links didn’t have http:// and were thus  the redirect was going to
    404 error

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,7 +45,7 @@ var grocers = L.geoJson(null, {
             });
 
             if (feature.properties.website){
-                content += "<tr><th>Website</th><td><a class='url-break' href='" + feature.properties.website + "' target='_blank'>" + feature.properties.website + "</a></td></tr>";
+                content += "<tr><th>Website</th><td><a class='url-break' href='http://" + feature.properties.website + "' target='_blank'>" + feature.properties.website + "</a></td></tr>";
             }
 
             content += "<table>";
